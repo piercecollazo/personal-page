9d0c8285a7e132e62c076844ba0a318086d9d95c
piercecollazo <piercecollazo@gmail.com>
2019-08-22 10:42:16 -0400

    Set up base react app and some components to start building the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import About from './components/About'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+
+    return (
+      <div className="App">
+        <Nav />
+        <About />
+        <Portfolio />
+        <Contact />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export default class Nav extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div>Placeholder text for Contact.js</div>
+        )
+    }
+}
