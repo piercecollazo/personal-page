0f8debbc506bd4f5cb48639306ff235479f6fea8
piercecollazo <piercecollazo@gmail.com>
2019-08-22 15:45:31 -0400

    further bootstrap improvements. Placeholder project images applied but proportions are borked :/

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-
+import Axolotl1 from '../img/axolotl-1.jpg'
+import Axolotl2 from '../img/axolotl-2.jpg'
+import Axolotl3 from '../img/axolotl-3.jpg'
 
 export default class Nav extends Component{
 
@@ -10,7 +12,7 @@ export default class Nav extends Component{
               <div className="row no-gutters">
                 <div className="col-lg-4 col-sm-6">
                   <a className="portfolio-box" href="img/portfolio/fullsize/1.jpg">
-                    <img className="img-fluid" src="img/portfolio/thumbnails/1.jpg" alt=""></img>
+                    <img className="img-fluid" src={Axolotl1} alt=""></img>
                     <div className="portfolio-box-caption">
                       <div className="project-category text-white-50">
                         Category
@@ -21,19 +23,35 @@ export default class Nav extends Component{
                     </div>
                   </a>
                 </div>
+
                 <div className="col-lg-4 col-sm-6">
                   <a className="portfolio-box" href="img/portfolio/fullsize/2.jpg">
-                    <img className="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt=""></img>
+                    <img className="img-fluid" src={Axolotl2} alt=""></img>
                     <div className="portfolio-box-caption">
                       <div className="project-category text-white-50">
                         Category
                       </div>
                       <div className="project-name">
-                        Project Name
+                        Placeholder Project 2
                       </div>
                     </div>
                   </a>
                 </div>
+
+                <div className="col-lg-4 col-sm-6">
+                  <a className="portfolio-box" href="img/portfolio/fullsize/2.jpg">
+                    <img className="img-fluid" src={Axolotl3} alt=""></img>
+                    <div className="portfolio-box-caption">
+                      <div className="project-category text-white-50">
+                        Category
+                      </div>
+                      <div className="project-name">
+                        Placeholder Project 3
+                      </div>
+                    </div>
+                  </a>
+                </div>
+
                 </div>
             </div>
           </section>
